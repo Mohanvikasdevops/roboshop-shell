3bc077a7f61a7ef4e3b02cf633a1344caf4340e2
Mohanvikas <vikas.gonugunta@gmail.com>
2026-05-12 15:07:09 +0530

    actions

---
+++ shipping.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-shell"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.109v.store
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then  
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+
+#By default shell will not execute, only executed when called
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+ 
+dnf install maven -y &>>$LOGS_FILE
+VALIDATE $? "Installing maven server"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "creating system user"  
+else
+    echo -e "Roboshop user already exist ... $Y skipping $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading shipping code"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/shipping.zip
+VALIDATE $? "Unzip shipping code"
+
+mvn clean package 
+VALIDATE $? "Cleaning Package"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOGS_FILE
+VALIDATE $? "Created  systemctl service"
+
+systemctl daemon-reload  &>>$LOGS_FILE
+systemctl enable shipping &>>$LOGS_FILE
+systemctl start shipping &>>$LOGS_FILE
+VALIDATE $? "start and enable shipping"
+
+dnf install mysql -y &>>$LOGS_FILE
+VALIDATE $? "Installing mysql server"
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOGS_FILE
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+VALIDATE $? "Setting Root Password"
+
+systemctl restart shipping &>>$LOGS_FILE
+VALIDATE $? "Restarted shipping"
+
+
+
+
+
+
