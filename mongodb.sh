1698f09a5ea9149e99c38981d08c6b92912a1110
Mohanvikas <vikas.gonugunta@gmail.com>
2026-05-12 08:50:31 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-shell"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then  
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+#By default shell will not execute, only executed when called
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOGS_FILE
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod &>>$LOGS_FILE
+VALIDATE $? "Restarted MongoDB"
+
+
+
