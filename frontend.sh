4a75c980d336a22d32ff8d75608b944da6e4e265
Mohanvikas <vikas.gonugunta@gmail.com>
2026-05-12 18:17:10 +0530

    actions

---
+++ frontend.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-shell"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.109v.store
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then  
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+
+#By default shell will not execute, only executed when called
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nginx -y &>>$LOGS_FILE
+VALIDATE $? "Disabling nginx server"
+
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling nginx server"
+ 
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing nginx server"
+
+systemctl enable nginx &>>$LOGS_FILE
+systemctl start nginx &>>$LOGS_FILE
+VALIDATE $? "start and enable nginx "
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "Moving to app directory"
+
+cp $SCRIPT_DIR/nginx.conf  /etc/nginx/nginx.conf &>>$LOGS_FILE
+VALIDATE $? "Created  systemctl service"
+
+systemctl restart nginx &>>$LOGS_FILE
+VALIDATE $? "Restarted nginx"
+
+
+
+
+
+
