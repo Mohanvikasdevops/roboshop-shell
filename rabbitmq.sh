68ff8dee1d89ace62c126f5f6aa6ba89ded26b1d
Mohanvikas <vikas.gonugunta@gmail.com>
2026-05-12 15:25:56 +0530

    actions

---
+++ rabbitmq.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-shell"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then  
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+
+#By default shell will not execute, only executed when called
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOGS_FILE
+VALIDATE $? "Copying rabbitmq Repo"
+
+dnf install rabbitmq-server -y &>>$LOGS_FILE
+VALIDATE $? "Installing rabbitmq server"
+
+systemctl enable rabbitmq-server &>>$LOGS_FILE
+VALIDATE $? "Enable rabbitmq-server"
+
+systemctl start rabbitmq-server &>>$LOGS_FILE
+VALIDATE $? "start rabbitmq-server"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "Add and setting user Permissions"
+
+
